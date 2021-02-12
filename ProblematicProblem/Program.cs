d9ce50dc47a8e45049b89071d92bccc27a9934d4
Norman Calhoun <shakauluz73@yahoo.com>
2021-02-12 00:35:00 -0500

    finished

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,34 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
 
-ProblematicProblem
+namespace ProblematicProblem
 {
-    Program class
+    class Program
     {
-        Random rng;        
-        static bool cont = true;
-        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
+        static string userResponse;  // ALL static keywords MUST BE IN THE CLASS
+        static bool cont = true;   /// same as above
+        static Random rng = new Random(); 
+        // user response used multiple times but with different rules see written code
+
+        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
 
         static void Main(string[] args)
         {
-            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
+
+            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+            userResponse = Console.ReadLine();
+
+            //if they do not want to do the program, exit:
+            if (userResponse.ToLower() != "yes")  
+            {
+                Console.WriteLine("GoodBye!");
+                return;
+            }
+            
+            
 
             Console.WriteLine();
 
+            //get thier name
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
 
             Console.WriteLine();
 
-            Console.Write("What is your age? ");
-            int userAge = Console.ReadLine();
+            Console.Write("What is your age? "); // gets the age
+            int userAge = int.Parse(Console.ReadLine());
 
             Console.WriteLine();
 
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
+            userResponse = Console.ReadLine();
 
-            if (seeList)
+
+
+            if (userResponse.ToLower() == "yes" || userResponse.ToLower() == "sure")
             {
+                //if they want to see activities first we loop through them here
                 foreach (string activity in activities)
                 {
                     Console.Write($"{activity} ");
@@ -37,17 +57,17 @@ ProblematicProblem
 
                 Console.WriteLine();
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                bool addToList = bool.Parse(Console.ReadLine());
+                string addToList = Console.ReadLine();
                 Console.WriteLine();
 
-                while (addToList)
+                while (addToList.ToLower() != "no")
                 {
                     Console.Write("What would you like to add? ");
                     string userAddition = Console.ReadLine();
 
                     activities.Add(userAddition);
 
-                    foreach (string activity activities)
+                    foreach (string activity in activities)
                     {
                         Console.Write($"{activity} ");
                         Thread.Sleep(250);
@@ -55,11 +75,11 @@ ProblematicProblem
 
                     Console.WriteLine();
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    string addToList = bool.Parse(Console.ReadLine());
+                    addToList = Console.ReadLine();   // use different name
                 }
             }
-            
-            while (cont)
+
+            do
             {
                 Console.Write("Connecting to the database");
 
@@ -69,7 +89,7 @@ ProblematicProblem
                     Thread.Sleep(500);
                 }
 
-                Console.WriteLine()
+                Console.WriteLine();
 
                 Console.Write("Choosing your random activity");
 
@@ -79,28 +99,34 @@ ProblematicProblem
                     Thread.Sleep(500);
                 }
 
-                Console.WriteLine()
+                Console.WriteLine();
 
                 int randomNumber = rng.Next(activities.Count);
 
-                string randomActivity = activities[randomNumber]
+                string randomActivity = activities[randomNumber];
 
-                if (userAge > 21 && randomActivity == "Wine Tasting")
+                if (userAge < 21 && randomActivity == "Wine Tasting")
                 {
+
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
 
                     activities.Remove(randomActivity);
 
-                    string randomNumber = rng.Next(activities.Count);
+                    randomNumber = rng.Next(activities.Count);
 
-                    string randomActivity = activities[randomNumber];
+                    randomActivity = activities[randomNumber];
                 }
 
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ")
-                ConsoleWriteLine();
-                bool cont = bool.Parse(Console.ReadLine());
-            }
+                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
+                userResponse = Console.ReadLine();  // taking users response 
+                if (userResponse.ToLower() != "redo") // only accepting the keep
+                {
+                    cont = false;
+                }
+
+            } while (cont);  // end of do while iteration
         }
     }
-}}
+}
